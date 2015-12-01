a38646788f6ed33718d6d0362f58da398ca6024e
Charles Moss <student@epicodus.com>
2015-12-01 14:37:29 -0800

    add first commit

---
+++ app/src/main/java/example/epic/myspace/MySpaceActivity.java
@@ -0,0 +1,25 @@
+package example.epic.myspace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MySpaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_space);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.tool_menu, menu);
+        return true;
+    }
+}
